c8d156a9602c6ab47ad20c32b0764668d88d5213
rakhentash <rakhentash@users.noreply.github.com>
2022-05-04 14:09:26 +0430

    Project Section from index page moved to partial view Projects

--- DevCard_MVC/Models/ErrorViewModel.cs
+++ DevCard_MVC/Models/ErrorViewModel.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 namespace DevCard_MVC.Models
 {
---
+++ DevCard_MVC/Models/Project.cs
@@ -0,0 +1,10 @@
+namespace DevCard_MVC.Models
+{
+    public class Project
+    {
+        public int Int { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Client { get; set; }
+    }
+}
